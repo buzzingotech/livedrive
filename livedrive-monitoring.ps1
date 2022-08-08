cf15e0e02943492a1cba5b8f2f22021fde576029
buzzingotech <109994024+buzzingotech@users.noreply.github.com>
2022-08-08 12:32:26 -0500

    Update livedrive-monitoring.ps1

--- livedrive-monitoring.ps1
+++ livedrive-monitoring.ps1
@@ -10,6 +10,7 @@ $livedrive = Get-Process Livedrive
 if (!$livedrive){
     # Start LiveDrive
     Start-Process ${env:ProgramFiles(x86)}\Livedrive\Livedrive.exe
+    Start-Sleep -Seconds 15
 }
 
 $process = $livedrive | Select-Object -Last 1
