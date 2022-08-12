945f3cd4a59ac7b87c2889c56f8467d0ae896250
buzzingotech <109994024+buzzingotech@users.noreply.github.com>
2022-08-11 19:19:48 -0500

    Update livedrive-monitoring.ps1

--- livedrive-monitoring.ps1
+++ livedrive-monitoring.ps1
@@ -4,6 +4,7 @@ $liveFolder = Test-Path $env:LOCALAPPDATA\Livedrive
 if (!$liveFolder){
     # Create folder
     New-Item -Path $env:LOCALAPPDATA\Livedrive -ItemType Directory
+    Write-Host "LocalApp LiveDrive folder not found. The folder was created"
 }
 
 Start-Transcript -Path "$env:LOCALAPPDATA\Livedrive\script.log"
@@ -11,6 +12,7 @@ Start-Transcript -Path "$env:LOCALAPPDATA\Livedrive\script.log"
 # Check if computer locked
 $logon = Get-Process LogonUI
 if ($null -ne $logon){
+    Write-Host "LogonUI process found running. Exiting script"
     exit 1
 }
 
@@ -19,6 +21,7 @@ $livedrive = Get-Process Livedrive
 if (!$livedrive){
     # Start LiveDrive
     Start-Process ${env:ProgramFiles(x86)}\Livedrive\Livedrive.exe
+    Write-Host "LiveDrive process was not running. Opening..."
     Start-Sleep -Seconds 30
 }
 
@@ -29,5 +32,7 @@ $img = Test-Path $env:LOCALAPPDATA\Livedrive\livedrive-screenshot.jpg
 if ($img){
     Remove-Item $env:LOCALAPPDATA\Livedrive\livedrive-screenshot.jpg
 }
+
+Write-Host "Starting executablee task"
 
 Start-Process $env:programdata\Syncro\bin\livedrive\Livedrive-monitor.exe -ArgumentList "$($process.Id)"
